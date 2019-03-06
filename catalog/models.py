e7b61029d3136e5ca345aa612bcbad55b8245363
Craig Brunengraber <brunengraber@hotmail.com>
2019-03-06 17:39:13 -0500

    stuck towards the end of part 5

--- catalog/models.py
+++ catalog/models.py
@@ -43,8 +43,13 @@ class Book(models.Model):
 
 
 
-class BookInstanceAdmin(admin.ModelAdmin):
-    list_filter = ('status', 'due_back')
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
 
     LOAN_STATUS = (
         ('m', 'maintenance'),

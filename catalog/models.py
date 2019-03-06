d02a551d0889d813ad110383c87d4b1a010aa50d
Craig Brunengraber <brunengraber@hotmail.com>
2019-03-05 22:08:47 -0500

    through part 3

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+import uuid
 # Create your models here.
 
 class Genre(models.Model):
@@ -35,9 +35,9 @@ class Book(models.Model):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
 
-import uuid
 
-class BookInstance(models.model):
+
+class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
     book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
@@ -65,3 +65,21 @@ class BookInstance(models.model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+    
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name}, {self.first_name}'
